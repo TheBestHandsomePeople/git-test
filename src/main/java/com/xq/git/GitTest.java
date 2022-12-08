d3d13cd7f8585ac28166b801903883dd563d3641
FangHuiFeng <FangHuiFeng@163.com>
2022-12-08 18:34:36 +0800

    first commit

---
+++ src/main/java/com/xq/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.xq.git;
+
+/**
+ * @author bestHandsomePeople
+ * @creat 2022-12-08-16:23
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("hello,git2");
+        System.out.println("hello,git3");
+        System.out.println("hello,git4");
+    }
+}
