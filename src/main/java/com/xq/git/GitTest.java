23ab914c7150cd9fa7fb1afb62fb37185d381e1b
TheBestHandsomePeople <112561737+TheBestHandsomePeople@users.noreply.github.com>
2022-12-09 16:51:52 +0800

    Update GitTest.java

--- src/main/java/com/xq/git/GitTest.java
+++ src/main/java/com/xq/git/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("gitee test");
         System.out.println("new test");
+        System.out.println("github test");
     }
 }
